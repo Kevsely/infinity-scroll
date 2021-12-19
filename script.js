1f8beda640a16797c3c6c2b5641d4dfe36a6acb6
Kevsely <elegis.sossou@yahoo.com>
2021-12-19 23:26:47 +0100

    Displaying photo gotten from api

--- script.js
+++ script.js
@@ -2,10 +2,48 @@ const access_key = "YKa-kPc9HlrvdDdbWlt_pdwPtsfNeU1-vo26o4Q_LpQ";
 const requestedImageCount = 20;
 const unsplashApi = `https://api.unsplash.com/photos/random/?client_id=${access_key}&count=${requestedImageCount}`;
 
-async function getPhoto() {
+let apiPhotos = [];
+
+//DOM Elements
+const image_container = document.getElementById("image_container");
+
+//Fetching API to get the photos
+async function getPhotos() {
     const response = await fetch(unsplashApi);
-    const data = await response.json();
-    console.log(data);
+    apiPhotos = await response.json();
+    displayPhotos();
 }
 
-getPhoto();
+//Helper func. to set image attributes
+function setAttributes(tag, attributes) {
+    let element = document.createElement(tag);
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+    return element;
+}
+
+//Display images got from API
+function displayPhotos() {
+    apiPhotos.forEach((photo) => {
+        //Creating anchor element
+        const anchor = setAttributes('a', {
+            href: photo.links.html,
+            target: "_blank",
+            title: photo.location.title
+        });
+
+        //Creating image element
+        const img = setAttributes('img', {
+            src: photo.urls.full,
+            alt: photo.location.title
+        });
+
+        anchor.appendChild(img);
+        image_container.appendChild(anchor);
+    })
+}
+
+
+
+getPhotos();
