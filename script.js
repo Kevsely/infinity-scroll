6d94653e5b9499c633931c02d1f73461b4ef6e8b
Kevsely <elegis.sossou@yahoo.com>
2021-12-19 10:52:58 +0100

    Fixing some mispelling in the getPhoto function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const access_key = YKa-kPc9HlrvdDdbWlt_pdwPtsfNeU1-vo26o4Q_LpQ;
-const requestedImageCount = 20;
-const unsplashApi = `https://api.unsplash.com/photos/random/?client_id=${access_key}?count=${requestedImageCount}`;
+const access_key = "YKa-kPc9HlrvdDdbWlt_pdwPtsfNeU1-vo26o4Q_LpQ";
+const requestedImageCount = 1;
+const unsplashApi = `https://api.unsplash.com/photos/random/?client_id=${access_key}&count=${requestedImageCount}`;
 
 async function getPhoto() {
     const response = await fetch(unsplashApi);
-    const data = response.json();
+    const data = await response.json();
     console.log(data);
 }
+
+getPhoto();
